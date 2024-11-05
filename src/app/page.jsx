45a3c33d89060497453854d2457a4ff7cd555733
Alon Mlievski <alonmlievski@gmail.com>
2024-11-05 23:06:29 +0200

    reroute homepage to login

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   useEffect(() => {
     if (!user) {
-      router.push('/auth/signup');
+      router.push('/auth/login');
     } else {
       router.push('/packages');
     }
